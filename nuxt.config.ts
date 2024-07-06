4952146252350a1d546f7d256c8c1dc46a64f91d
Bruno gomez <br1_gio@hotmail.com>
2024-07-06 14:13:55 -0600

    feat: add nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  ssr: false,
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
